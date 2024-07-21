698b7ec1d88fb8902dc852108da29edbcf2340ce
katic-nemanja <katic.nemanja@gmail.com>
2024-07-22 00:39:49 +0200

    mobilna verzija

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
+function TurnOff(){
+    let img = document.querySelector("#errorIMG");
+    img.style.display= "none";
+}
+
+function TurnOn(){
+    let img = document.querySelector("#errorIMG");
+    img.style.display= "block";
+}
+
 function ValidateEmail()
 {
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     var mejl = document.getElementById("email").value;
-    // let paragraf = document.querySelector("#demo");
+    let paragraf = document.querySelector("#demo");
     // paragraf.textContent = mejl.textContent;
 
     console.log("mejl:" + mejl);
     // console.log("paragraf:" + paragraf.textContent);
     if (mejl.match(mailformat))
     {
-        alert("Valid email address!");
-        return true;
+        
+        /*alert("Valid email address!");
+        return true;*/
     }
     else
     {
-        alert("You have entered an invalid email address!");
-        return false;
+        paragraf.innerHTML = "Please provide a valid email";
+        TurnOn();
+        // alert("You have entered an invalid email address!");
+        // return false;
     }
+}
+
+
+function Reset(){
+    let paragraf = document.querySelector("#demo");
+    TurnOff();
+    paragraf.textContent = "";
 }
